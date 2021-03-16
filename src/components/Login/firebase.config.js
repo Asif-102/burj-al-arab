7af329fb12d31437649f38b795678d6b27ee1593
Asif2535 <aiqbal517@gmail.com>
2021-03-17 01:27:49 +0600

    configure google sign in authentication

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBK6NMFobJPcGx-DY9sGvf99wAbJlBNtuY",
+    authDomain: "burj-al-arab-6d876.firebaseapp.com",
+    projectId: "burj-al-arab-6d876",
+    storageBucket: "burj-al-arab-6d876.appspot.com",
+    messagingSenderId: "1043658955148",
+    appId: "1:1043658955148:web:61531f441bde80183d9ea9"
+  };
+
+  export default firebaseConfig;
